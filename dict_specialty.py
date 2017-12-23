339b74dd9ec1ee8d3772b4a453da75852a03264a
“zuoxiaojun <zuoxiaojun111@163.com>
2017-12-23 20:23:16 +0800

    dict

---
+++ dict_specialty.py
@@ -0,0 +1,12 @@
+# 请设计一个dict，可以根据分数来查找名字，已知成绩如下：
+# Adam: 95,
+# Lisa: 85,
+# Bart: 59.
+
+d = {
+    95:'Adam',
+    85:'Lisa',
+    59:'Bart'
+}
+a = input("input score:")
+print(d.get(int(a)))   # a 默认应该是str型，故用int函数转换一下
